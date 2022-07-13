0c43e5f661642c258ea72359f4bb7e26fafd5441
Inuri-99 <96541@fhss.sjp.ac.lk>
2022-07-13 21:32:40 +0530

    Category Page

---
+++ frontend/src/pages/ProductList.js
@@ -0,0 +1,83 @@
+import styled from "styled-components"
+import Announcement from "../components/Announcement"
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar"
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+
+const Container = styled.div ``;
+
+const Title = styled.h1 `
+    margin: 20px;
+`;
+
+const FilterContainer = styled.div `
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Filter = styled.div `
+    margin:10px;
+`;
+
+const FilterText = styled.span `
+    font-size: 15px;
+    font-weight:600;
+    margin-right:20px;
+`;
+
+const Select = styled.select `
+    padding: 10px;
+    margin-right: 20px;
+`;
+
+const Option = styled.option `
+
+`;
+
+const ProductList = () => {
+    return (
+        <Container>
+            <Navbar/>
+            <Announcement/>
+            <Title>Pizzas</Title>
+            <FilterContainer>
+                <Filter>
+                <FilterText>Pan & Other Crusts:</FilterText>
+                <Select>
+                    <Option disabled selected>PIZZA</Option>
+                    <Option>Classic</Option>
+                    <Option>Signature</Option>
+                    <Option>Favourite</Option>
+                    <Option>Supreme</Option>
+                    <Option>Thin Crust</Option>
+                </Select>
+                <Select>
+                    <Option disabled selected>
+                        APPETIZERS & OTHERS
+                    </Option>
+                    <Option>Appetizers</Option>
+                    <Option>Rice</Option>
+                    <Option>Pastas</Option>
+                    <Option>Deserts</Option>
+                    <Option>Beverages</Option>
+                </Select>
+                </Filter>
+                
+                <Filter><FilterText>Sort Products:</FilterText>
+                <Select>
+                    <Option selected>Newest</Option>
+                    <Option>Price (asc)</Option>
+                    <Option>Price (desc)</Option>
+                   
+                </Select>
+                </Filter>
+            </FilterContainer>
+            <Products/>
+            <Newsletter/>
+            <Footer/>
+        </Container>
+    )
+}
+
+export default ProductList

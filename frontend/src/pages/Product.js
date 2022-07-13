13677f6bcfc1e995366d001e9bb1533503fa24e8
Inuri-99 <96541@fhss.sjp.ac.lk>
2022-07-14 02:29:09 +0530

    Styled Components Converting Responsive Design

--- frontend/src/pages/Product.js
+++ frontend/src/pages/Product.js
@@ -4,6 +4,7 @@ import Announcement from "../components/Announcement"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 import Newsletter from "../components/Newsletter"
+import { mobile } from "../responsive"
 
 const Container = styled.div `
 
@@ -12,6 +13,7 @@ const Container = styled.div `
 const Wrapper = styled.div `
     padding: 50px;
     display: flex;
+    ${mobile({padding:"10px", flexDirection:"column"})}
 `;
 
 const ImgContainer = styled.div `
@@ -22,11 +24,13 @@ const Image = styled.img `
     width: 70% ;
     height: 50vh ;
     object-fit: cover;
+    ${mobile({height:"40vh"})}
 `;
 
 const InfoContainer = styled.div `
     flex:1;
     padding: 0px 50px;
+    ${mobile({padding:"10px"})}
 `;
 
 const Title = styled.h1 `
@@ -49,6 +53,7 @@ const FilterContainer = styled.div `
     margin: 30px 0px;
     display: flex;
     justify-content: space-between;
+    ${mobile({width:"100%"})}
 `;
 
 const Filter = styled.div `
@@ -81,6 +86,7 @@ const AddContainer = styled.div `
     display: flex;
     align-items: center;
     justify-content: space-between;
+    ${mobile({width:"100%"})}
 `;
 const AmountContainer = styled.div `
     display: flex;
@@ -116,8 +122,8 @@ const Button = styled.button `
 const Product = () => {
     return (
         <Container>
-            <Navbar/>
             <Announcement/>
+            <Navbar/>
             <Wrapper>
                 <ImgContainer>
                     <Image src="https://adminsc.pizzahut.lk/images/mainmenu/cheeselovers8af791bb96d1465aa31f4cb22d642068.jpg"/>

074486a0ee6c2ae0770259865187749e6b0bc70c
Inuri-99 <96541@fhss.sjp.ac.lk>
2022-07-13 20:43:34 +0530

    Create Pure React Slider

---
+++ frontend/src/data.js
@@ -0,0 +1,27 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "http://riverviewspizza.com/wp-content/uploads/2020/04/girl-eating-pizza-1024x994.png",
+        title: "WORLD OF UNPARALLELED FLAVORS",
+        desc: "We are celebrating the return of a fan-favourite pizza with fresh new flavours.",
+        desc2: "COME IN TODAY AND ENJOY!",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://www.pngplay.com/wp-content/uploads/9/Junk-Food-PNG-Pic-Background.png",
+        title: "YOUR EVENT WITH US",
+        desc: "we have great offers with amazing prices every week.",
+        desc2: "CHECK IT OUT TODAY!",
+        bg: "fbf0f4"
+    },    
+    {
+        id: 3,
+        img: "https://png.monster/wp-content/uploads/2022/03/png.monster-890.png" ,
+        title: "ORDER YOUR BEST PIZZADO ANYTIME.",
+        desc: "Hey! Our Delicious pizzas are waiting for you, we are always near to you with fresh item of pizzas",
+        desc2: "LET'S START YOUR ORDER NOW!",
+        bg: "fcf1ed"
+    }    
+      
+];
